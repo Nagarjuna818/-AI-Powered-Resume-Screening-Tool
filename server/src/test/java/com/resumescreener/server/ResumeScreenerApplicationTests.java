cd42cfa1a30ff800e48725decdb5f7251865ab97
Gopichand Mogaparthi <gopichandmogaparthi@Mac.lan>
2025-04-24 16:20:45 -0400

    Added Spring Boot Backend Structure

---
+++ server/src/test/java/com/resumescreener/server/ResumeScreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumescreener.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeScreenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
