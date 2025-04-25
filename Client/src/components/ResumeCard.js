ceacc71329cbc0ab0be0ce6c8d5aa0818f8c866f
nagarjuna <nagarjunar554@gmail.com>
2025-04-25 11:25:14 -0700

    updated UI

--- Client/src/components/ResumeCard.js
+++ Client/src/components/ResumeCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ResumeCard.css';
 
 function ResumeCard({ file, onFileChange, onRemove, resumeText, setResumeText }) {
