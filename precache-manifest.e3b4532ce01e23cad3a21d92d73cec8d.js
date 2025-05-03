d085f944685bf9d0307f84c095f29b0d74657c17
nagarjuna <nagarjunar554@gmail.com>
2025-05-03 14:46:59 -0700

    Updates

---
+++ precache-manifest.e3b4532ce01e23cad3a21d92d73cec8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be8e326ec3859d4b75a9b3874bb5a65",
+    "url": "/-AI-Powered-Resume-Screening-Tool/index.html"
+  },
+  {
+    "revision": "67c2c8b02fe42003ab1f",
+    "url": "/-AI-Powered-Resume-Screening-Tool/static/css/main.90ba1d97.chunk.css"
+  },
+  {
+    "revision": "3779c2ead5f7feedd13b",
+    "url": "/-AI-Powered-Resume-Screening-Tool/static/js/2.9ed52326.chunk.js"
+  },
+  {
+    "revision": "b14cf38e36b679bf3c7c",
+    "url": "/-AI-Powered-Resume-Screening-Tool/static/js/3.bb0829b5.chunk.js"
+  },
+  {
+    "revision": "67c2c8b02fe42003ab1f",
+    "url": "/-AI-Powered-Resume-Screening-Tool/static/js/main.b586109c.chunk.js"
+  },
+  {
+    "revision": "5f739f025915573625fd",
+    "url": "/-AI-Powered-Resume-Screening-Tool/static/js/runtime~main.d8459714.js"
+  },
+  {
+    "revision": "4e73f56c7db69f9fda597caa3999792c",
+    "url": "/-AI-Powered-Resume-Screening-Tool/static/media/img.4e73f56c.png"
+  }
+]);
